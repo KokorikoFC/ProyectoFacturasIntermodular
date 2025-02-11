dbd92d37a2f9d48dff654b7539b9d8eb0aa198c8
Yilin <yilin.zh135@gmail.com>
2025-02-11 22:44:33 +0100

    Update login style

--- app/src/main/java/com/example/proyectofacturasintermodular/ui/screens/loginScreen.kt
+++ app/src/main/java/com/example/proyectofacturasintermodular/ui/screens/loginScreen.kt
@@ -2,9 +2,11 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
@@ -12,57 +14,83 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
+import com.example.proyectofacturasintermodular.R
 import com.example.proyectofacturasintermodular.ui.theme.*
 import com.example.proyectofacturasintermodular.viewmodel.AuthViewModel
 
 
-
 @Composable
 fun LoginScreen(navHostController: NavHostController, authViewModel: AuthViewModel) {
-    val context = LocalContext.current
     val email = authViewModel.email.value
     val password = authViewModel.password.value
     val errorMessage = authViewModel.errorMessage.value
 
-    // Función para manejar el login
-    fun login() {
-        authViewModel.login(
-            onSuccess = {
-                // Si el login es exitoso, navegamos a otra pantalla
-                navHostController.navigate("addBill")
-            },
-            onFailure = { message ->
-                // Si el login falla, mostramos el mensaje de error
-                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-            }
-        )
-    }
-
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Gray)
-        ,
-        contentAlignment = Alignment.Center
-
+            .background(Gray),
     ) {
 
+        Image(
+            painter = painterResource(id = R.drawable.background2),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 25.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Inicia sesión",
+                    style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold, color = Beige)
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "Debes iniciar sesión para poder registrar una factura",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Beige
+                    ),
+                    textAlign = TextAlign.Justify,
+                    lineHeight = 30.sp
+                )
+            }
+        }
+
+        // Formulario de login
         Box(
             modifier = Modifier
-                .fillMaxHeight(0.7f)
+                .fillMaxHeight(0.65f)
                 .align(Alignment.BottomCenter)
-                .clip(RoundedCornerShape(topStart = 35.dp, topEnd = 35.dp))
+                .clip(RoundedCornerShape(topStart = 35.dp, topEnd = 30.dp))
                 .background(Beige, shape = RoundedCornerShape(topStart = 35.dp, topEnd = 35.dp))
-                .padding(bottom = 60.dp)
+                .padding(bottom = 40.dp)
+                .zIndex(1f) // Aseguramos que esté encima del círculo
         ) {
             Column(
                 modifier = Modifier
@@ -71,52 +99,143 @@ fun LoginScreen(navHostController: NavHostController, authViewModel: AuthViewMod
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = "Iniciar Sesión",
-                    style = TextStyle(
-                        fontSize = 28.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Gray,
+                Spacer(modifier = Modifier.height(30.dp))
+
+                // Campo de correo electrónico
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Start)
+                        .padding(bottom = 10.dp)
+                ) {
+                    Text(
+                        "Correo electrónico",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Gray
+                        )
                     )
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                TextField(
-                    value = email,
-                    onValueChange = { authViewModel.email.value = it },
-                    label = { Text("Correo Electrónico") },
-                    singleLine = true,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                TextField(
-                    value = password,
-                    onValueChange = { authViewModel.password.value = it },
-                    label = { Text("Contraseña") },
-                    singleLine = true,
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(onClick = { login() }) {
-                    Text("Iniciar sesión")
                 }
 
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Gray, shape = RoundedCornerShape(8.dp))
+                        .padding(2.dp)
+                ) {
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { authViewModel.email.value = it },
+                        placeholder = { Text("Correo Electrónico") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = Color.Transparent,
+                            focusedBorderColor = Color.Transparent,
+                            focusedTextColor = Beige,
+                            unfocusedTextColor = Beige,
+                            focusedPlaceholderColor = Color.Gray,
+                            unfocusedPlaceholderColor = Color.Gray,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent
+                        )
+                    )
+                }
 
+                Spacer(modifier = Modifier.height(30.dp))
 
+                // Campo de contraseña
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Start)
+                        .padding(bottom = 10.dp)
+                ) {
+                    Text(
+                        "Contraseña",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Gray
+                        )
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Gray, shape = RoundedCornerShape(8.dp))
+                        .padding(2.dp)
+                ) {
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { authViewModel.password.value = it },
+                        placeholder = { Text("Contraseña") },
+                        singleLine = true,
+                        visualTransformation = PasswordVisualTransformation(),
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = Color.Transparent,
+                            focusedBorderColor = Color.Transparent,
+                            focusedTextColor = Beige,
+                            unfocusedTextColor = Beige,
+                            focusedPlaceholderColor = Color.Gray,
+                            unfocusedPlaceholderColor = Color.Gray,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Column(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if (errorMessage.isNotEmpty()) {
+                        Text(
+                            text = errorMessage,
+                            color = Color.Red,
+                            style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                // Botón de login
+                Button(
+                    onClick = {
+                        authViewModel.login(
+                            onSuccess = {
+                                // Navegar a la siguiente pantalla si el login es exitoso
+                                navHostController.navigate("addBill")
+                            },
+                            onFailure = { message ->
+                                // Manejo de error
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = Red),
+                    shape = RoundedCornerShape(8.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    Text(
+                        text = "Iniciar sesión",
+                        color = Gray,
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Gray
+                        )
+                    )
+                }
             }
-        }
-
-
-        // Mensaje de error
-        if (errorMessage.isNotEmpty()) {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = errorMessage,
-                color = Color.Red,
-                style = MaterialTheme.typography.bodySmall
-            )
         }
     }
 }
